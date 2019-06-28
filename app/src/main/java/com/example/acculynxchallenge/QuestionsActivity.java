202ac1d3cad92def8485046f8a87097f658ab6fa
Max Gura <msgur@DESKTOP-933AUUJ.(none)>
2019-06-27 21:15:20 -0500

    Created XML for main activity and implementated Retrofit

---
+++ app/src/main/java/com/example/acculynxchallenge/QuestionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.acculynxchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.questions_activity);
+    }
+}
