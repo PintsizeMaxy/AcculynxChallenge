361a15ba558707d216039bcb31221b8f86c0800b
Max Gura <msgur@DESKTOP-933AUUJ.(none)>
2019-06-28 20:09:41 -0500

    Created AnswersActivity and parsed JSON to find AnswerIDs

--- app/src/main/java/com/example/acculynxchallenge/AnswersActivity.java
+++ app/src/main/java/com/example/acculynxchallenge/AnswersActivity.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -13,6 +20,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class AnswersActivity extends AppCompatActivity {
+
+    ArrayList<AnswerModel> mList = new ArrayList<>();
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +66,22 @@ public class AnswersActivity extends AppCompatActivity {
     }
 
     private void readAnswers(String response){
+        try{
+            JSONObject obj = new JSONObject(response);
+            JSONArray array = obj.getJSONArray("items");
+            for (int i = 0; i < array.length(); i++){
+                AnswerModel model = new AnswerModel();
+                JSONObject jObj = array.getJSONObject(i);
+                model.setAnswer_id(jObj.getInt("answer_id"));
+                model.setIs_accepted(jObj.getBoolean("is_accepted"));
+                model.setScore(jObj.getInt("score"));
+                mList.add(model);
+            }
 
+            mRecyclerView = (RecyclerView) findViewById(R.id.questionList);
+
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
     }
 }
